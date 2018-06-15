c771dfccb6578519ea210968a35c8a74670049ec
jonathanko45 <39708300+jonathanko45@users.noreply.github.com>
2018-06-14 23:42:51 -0400

    Update Arduino_Car.ino

--- Arduino_Car/Arduino_Car.ino
+++ Arduino_Car/Arduino_Car.ino
@@ -8,12 +8,12 @@ long duration, distance;
 void setup() {
   // initalizing the pin locations and input/output
   pinMode(8, INPUT);
-  pinMode(2, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(7, OUTPUT);
+  pinMode(2, OUTPUT);   //line follower LED
+  pinMode(4, OUTPUT);   //left motor
+  pinMode(7, OUTPUT);   //right motor
   pinMode(12, INPUT);
   pinMode(13, OUTPUT);
-  pinMode(A0, INPUT);
+  pinMode(A0, INPUT);   //light sensor
   // initalizing the bitrate per second between the arduino and bluetooth
   Serial.begin(9600);
   mySerial.begin(9600);
@@ -24,6 +24,7 @@ void loop() {
   while (mySerial.available()){
     // reads the input and puts it into a variable
     a = mySerial.read();
+    //switches mode to line follower
     if (a == 'J')
     {
       mode = 1;
@@ -33,7 +34,9 @@ void loop() {
     int direct = 0;
     
     while (mode == 1){
+      // turns on led
       digitalWrite(2, HIGH);
+      // turns on light sesor and reads in its input
       detect = digitalRead(A0);
       a = mySerial.read();
       if (a == 'H')
@@ -42,8 +45,12 @@ void loop() {
       }
       if (detect)
       {
+        // drives the car to the right
+        // left motor is forwards
         digitalWrite(4, HIGH);
+        // right motor is bacwards
         digitalWrite(7, LOW);
+        // second num sets the speed of the motors
         analogWrite(5, 80);
         analogWrite(6, 120);
       }
@@ -87,7 +94,7 @@ void loop() {
       }
     }
     */
-    
+    // reads in the input and runs the motor appropriately
     if (a=='F')
     {
       digitalWrite(4, LOW);
@@ -102,28 +109,28 @@ void loop() {
       analogWrite(5, 130);
       analogWrite(6, 130);
     }
-    else if (a=='L')
+    else if (a == 'L')
     {
       digitalWrite(4, HIGH);
       digitalWrite(7, LOW);
       analogWrite(5, 130);
       analogWrite(6, 130);
     }   
-    else if (a=='B')
+    else if (a == 'B')
     {
       digitalWrite(4, HIGH);
       digitalWrite(7, HIGH);
       analogWrite(5, 255);
       analogWrite(6, 255);
     }
-    else if (a =='G')
+    else if (a == 'G')
     {
       digitalWrite(4, LOW);
       digitalWrite(7, LOW);
       analogWrite(5, 150);
       analogWrite(6, 255);
     }
-    else if (a =='I')
+    else if (a == 'I')
     {
       digitalWrite(4, LOW);
       digitalWrite(7, LOW);
